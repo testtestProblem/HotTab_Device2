1f13a09fbf69843bbbe7fb942d74aa3323843de0
testtestproblem <tommyapple321@gmail.com>
2023-09-22 18:42:40 +0800

    Add project files.

---
+++ CollectDataAP/DeviceState.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectDataAP
+{
+    class DeviceState
+    {
+        [DllImport(@"WMIO2.dll")]
+        public static extern bool SetDevice(byte uiValue);
+
+        [DllImport(@"WMIO2.dll")]
+        public static extern bool GetDevice(out byte uiValue);
+
+        [DllImport(@"WMIO2.dll")]
+        public static extern bool SetDevice2(byte uiValue);
+
+        [DllImport(@"WMIO2.dll")]
+        public static extern bool GetDevice2(out byte uiValue);
+
+
+        private enum deviceState { };
+
+        //0: The device power off     1: The device power on
+        //lowwer byte
+        //Bit7      Bit6        Bit5        Bit4        Bit3        Bit2    Bit1            Bit0
+        //AllLED    WebCamRear  Bluetooth   -           -           GPS     Gobi3G          Wifi
+        //upper byte
+        //Bit7      Bit6        Bit5        Bit4        Bit3        Bit2    Bit1            Bit0
+        //-         -           Expand COM  Expand USB  GPS Antenna RFID    WebCam Front    Barcode
+        public uint GetDeviceState()
+        {
+            byte temp;
+            uint devicestate = 0;
+
+            GetDevice(out temp);
+            devicestate =  temp;
+
+            GetDevice2(out temp);
+            devicestate += (((uint)temp) << 8);
+
+            return devicestate;
+        }
+
+        //TODO...
+        public string ParseDeviceStateCode(uint data)
+        {
+            string deviceState = "";
+
+            if ((data & 0x0001) == 0x0001) deviceState += "";
+
+
+                return deviceState;
+        }
+
+        //TODO: do more personalize
+        public bool SetDeviceState(uint data)
+        {
+            byte data1 = (byte)data;
+            byte data2 = (byte)(data>>8);
+
+            if(SetDevice(data1) == false) return false;
+            if(SetDevice2(data2) == false) return false;
+
+            return true;
+        }
+
+    }
+}
---
+++ CollectDataAP/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.AppService;
+using Windows.Foundation.Collections;
+
+namespace CollectDataAP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tommy_Start();
+
+            DeviceState deviceState = new DeviceState();
+            uint deviceStateData = 0;
+            string choice = "";
+
+            Console.WriteLine("Choose the number you want:\n[1] Get device state\n[2] Set device state\n[0] Exit");
+            while ((choice = Console.ReadLine()) != "0") 
+            { 
+                if(choice=="1")
+                {
+                    deviceStateData = deviceState.GetDeviceState();
+                    Console.WriteLine("Device state code: " + deviceStateData.ToString());
+                }
+                else if(choice == "2")
+                {
+                    string temp = Console.ReadLine();
+
+                    deviceState.SetDeviceState((uint)Convert.ToInt32(temp));
+                    Console.WriteLine("OK");
+                }
+            }
+        }
+
+        static public void Tommy_Start()
+        {
+            Console.WriteLine("------------------------------------------------------------");
+            Console.WriteLine("|   ========     =====     |\\   /|    |\\   /|    \\\\    //  |");
+            Console.WriteLine("|      ||       //   \\\\    |\\   /|    |\\   /|     \\\\  //   |");
+            Console.WriteLine("|      ||       ||   ||    | \\ / |    | \\ / |      \\\\//    |");
+            Console.WriteLine("|      ||       \\\\   //    |  |  |    |  |  |       ||     |");
+            Console.WriteLine("|      ||        =====     |  |  |    |  |  |       ||     |");
+            Console.WriteLine("------------------------------------------------------------");
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+    }
+
+    class Connect2UWP
+    {
+        private AppServiceConnection connection = null;
+
+        public async void InitializeAppServiceConnection()
+        {
+            connection = new AppServiceConnection();
+            connection.AppServiceName = "SampleInteropService";
+            connection.PackageFamilyName = Package.Current.Id.FamilyName;
+            //connection.RequestReceived += Connection_RequestReceived;
+            //connection.ServiceClosed += Connection_ServiceClosed;
+
+            AppServiceConnectionStatus status = await connection.OpenAsync();
+            if (status != AppServiceConnectionStatus.Success)
+            {
+                // something went wrong ...
+                Console.WriteLine(status.ToString());
+                Console.ReadLine();
+                //this.IsEnabled = false;
+            }
+        }
+
+        public async void SendData2UWP(string data)
+        {
+            // ask the UWP to calculate d1 + d2
+            ValueSet request = new ValueSet();
+            request.Add("D1", (string)data);
+            //request.Add("D2", (double)2);
+            AppServiceResponse response = await connection.SendMessageAsync(request);
+            //double result = (double)response.Message["RESULT"];
+        }
+
+
+    }
+}
